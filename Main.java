c1cc1558817f03f54ccafb1cebb94dba30de460e
DanielParedes23 <dmparedes12@espe.edu.ec>
2024-11-26 18:34:59 -0500

    first commit

---
+++ Main.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author danie
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Persona persona = new Persona();
+        persona.nombre = "Daniel";
+        persona.apellido = "Paredes";
+        persona.edad = 19;
+        persona.diasparacumpleaños = 250;
+        
+        persona.hablar();
+        
+        
+        
+       
+        
+        
+        
+        
+    }
+   
+}
---
+++ Persona.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author daniel paredes
+ */
+public class Persona {
+    String nombre; 
+    String apellido;
+    int edad;
+    int diasparacumpleaños;
+
+
+public void hablar () {
+System.out.println("la persona " +   nombre  + "  " + apellido + " de edad   " + edad + " esta a estos dias para cumplir anios   " + diasparacumpleaños + " y  " + "estudia telecomunicaciones" );
+    
+    
+}
+}
